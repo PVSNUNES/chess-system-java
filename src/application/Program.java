91105b11f1e73e6bf3b2b9efdbda815659670fcf
PVSNUNES <pepedronunes@gmail.com>
2024-08-19 18:55:13 -0300

    Handling exceptions and cleaning sreen

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,6 +1,8 @@
 package application;
 
 import chess.ChessMatch;
+import java.util.InputMismatchException;
+import chess.ChessException;
 import chess.ChessPiece;
 import chess.ChessPosition;
 import java.util.Scanner;
@@ -15,17 +17,27 @@ public class Program {
 		
 		
 		while(true) {
-			UI.printBoard(chessMatch.getPieces());
-			System.out.println();
-			System.out.print("Source: ");
-				ChessPosition source = UI.readChessPosition(sc);
-			
-			System.out.println();
-			System.out.print("Target: ");
-			ChessPosition target = UI.readChessPosition(sc);
-			
-			ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
-					
+				try {
+				UI.clearScreen();
+				UI.printBoard(chessMatch.getPieces());
+				System.out.println();
+				System.out.print("Source: ");
+					ChessPosition source = UI.readChessPosition(sc);
+				
+				System.out.println();
+				System.out.print("Target: ");
+				ChessPosition target = UI.readChessPosition(sc);
+				
+				ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
+				}
+			catch(ChessException e) {
+				System.out.println(e.getMessage());
+				sc.nextLine();
+			}
+				catch(InputMismatchException e) {
+					System.out.println(e.getMessage());
+					sc.nextLine();
+				}
 		}
 		
 	}
--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -43,7 +43,7 @@ public class ChessMatch {
 	
 	private void validateSourcePosition(Position position) {
 		if(!board.thereIsAPiece(position)) {
-			throw new BoardException("There is no pice on source position");
+			throw new ChessException("There is no pice on source position");
 		}
 	}
 
